c177fcf871508b086fecee6848aab8a8050d25c1
KarenMC3 <karenn1129@gmail.com>
2017-10-07 09:41:19 -0500

    Add files via upload

---
+++ Curso-bd2.sql
@@ -0,0 +1,4 @@
+--Clase de hoy sabado 7 de octubre
+create table almacen(numero_almacen integer, ubicacion_almacen varchar2(80),
+                    constraint pk_na primary key(numero_almacen)
+                    );
