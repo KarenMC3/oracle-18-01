dc468ccde5302913c479e68c87d203b78f9c4645
KarenMC3 <karenn1129@gmail.com>
2017-10-07 10:45:53 -0500

    Add files via upload

--- Curso-bd2.sql
+++ Curso-bd2.sql
@@ -2,3 +2,19 @@
 create table almacen(numero_almacen integer, ubicacion_almacen varchar2(80),
                     constraint pk_na primary key(numero_almacen)
                     );
+                    
+--Vamos a generar un procedimiento almacenado para guardar una entidad o registro de tipo almacen
+
+create or replace procedure guardar_almacen(mi_id IN integer, mi_ubicacion IN varchar2)
+                            as
+                            begin
+                            --Aqui la logica del procedimiento
+                          insert into almacen values(mi_id, mi_ubicacion);
+                          end;
+                          /
+        --Vamos a comprobar que funciona nuestro procedimiento
+        begin
+        guardar_almacen(1,'Plymouth');
+        end;
+        /
+        select * from almacen;
